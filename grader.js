558cf366e169cefefa5939f649499bfc4417f2a5
maxicecilia <maxicecilia@gmail.com>
2013-07-10 01:35:20 -0300

    update checks and grader

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -55,6 +56,20 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var response2console = function(result, response) {
+      if (result instanceof Error) {
+          console.error('Error: ' + util.format(response.message));
+      } else {
+          console.error("Wrote %s", HTMLFILE_DEFAULT);
+          fs.writeFileSync(HTMLFILE_DEFAULT, result);
+      }
+};
+
+var assertURL = function(url) {
+  console.log("assertURL: " + url);
+  return rest.get(url).on('complete', response2console);
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,6 +80,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_link>', 'link to url', clone(assertURL), "")
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
